ccb347155465188e2ca63971c8452e6d01c22e40
pavithrakakarla <106824919+pavithrakakarla@users.noreply.github.com>
2022-06-30 15:20:31 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+x = 2
+y = 3
+
+
+# In[10]:
+
+
+z = x+y
+print(z)
+
+
+# ## Rules for declaring variables in python:    
+
+# In[ ]:
+
+
+1. No namespaces whle declaring variables in python
+
+
+# In[14]:
+
+
+first name = 'p'
+   print(first name)
+
+
+# In[13]:
+
+
+firstname='p'
+print(firstname)
+
+
+# In[ ]:
+
+
+2. A variable name should not start with a number **
+
+
+# In[16]:
+
+
+1name='v'
+print(1name)
+
+
+# In[17]:
+
+
+name1='v'
+print(name1)
+
+
+# In[ ]:
+
+
+3. A variable name should not contain any special characters. 
+ like @ $ % & ^
+
+
+# In[18]:
+
+
+name@='n'
+print(name@)
+
+
+# ## Introduction to datatypes in python
+
+# In[ ]:
+
+
+1. string ---->str
+
+2.Numbers-----> integers(int) , floats (float)=======> decimals
+
+3. list----->list
+
+4.tuple----->tuple
+
+5.dictionary----->dict
+
+
+# ### Classification of the datatypes:
+
+# In[ ]:
+
+
+Datatypes are broadly classified into different categories:
+    
+    1. mutable datatype---->which we can edit or alter------>flexible data types
+    
+    2. immutable datatype---->which we cannot edit or alter---->fixed data types
+
+
+# #### Introduction to string datatype:
+
+# In[ ]:
+
+
+Definition     : A string is nothing but a series of characters declared in quotes.
+    
+Classification : it is classified as an immutable datatype.
+
+How to declare/ define the string........> 3 possible approaches.
+
+1. single quote
+
+2. double quote
+
+3. triple quote
+
+
+# In[20]:
+
+
+name1 = 'ap' # single quote
+print(name1)
+
+
+# In[21]:
+
+
+name2 = "ap" # double quote
+print(name2)
+
+
+# In[22]:
+
+
+name3 = """ap""" # trple quote
+print(name3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
